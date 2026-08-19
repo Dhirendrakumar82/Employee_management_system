a80d10d36f3353d13668b342c57595b94a01d943
Dhirendra Kumar <dhirendrak2348@gmail.com>
2026-08-19 12:16:05 +0530

    Fix allowed hosts for Render

--- employee_management/settings.py
+++ employee_management/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_hlcrve0=8o%9g8_530m*%vgtmn-&t%-q&0g1j9^nepjq(ni(1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "employee-management-system-vrna.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
